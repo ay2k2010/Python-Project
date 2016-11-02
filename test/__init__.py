2c709a225fbb4efb66eca85f1544d0ad7b76175c
Philip Stachura <philip.stachura@unoworks.ca>
2016-11-02 15:27:33 -0700

    This is a simple test branch were I will become familiar with using python and git

---
+++ test/__init__.py
@@ -0,0 +1,18 @@
+# Triangle Calculator:
+#
+#   A
+#   |\
+#  a| \c
+#   |__\
+#  B  b  C
+import math
+A = 0
+a = 0
+B = 0
+b = 0
+C = 0
+c = 0
+a = c*math.sin(A)
+a = b/math.tan(A)
+print("Hello World")
+print("Test")
