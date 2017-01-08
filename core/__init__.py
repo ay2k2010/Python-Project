17c1cb29e4529a400067bdb5f990eee089c6c241
Philip Stachura <philip.stachura@unoworks.ca>
2017-01-07 17:18:10 -0800

    organized tests

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,10 @@
 from core.advanced_shapes import *
 from shapes.test_shapes import *
 
-add_object(ColourSquare(Point(20, 20), 10), True, True, True)
-add_object(Spaceship(Point(50, 50), 10, 0.1, 0.4, 8, 0.3), True, False, True)
 
+def test_shapes():
+    add_object(ColourSquare(Point(20, 20), 10), True, True, True)
+    add_object(Spaceship(Point(50, 50), 10, 0.1, 0.4, 8, 0.3), True, False, True)
+
+test_shapes()
 loop()
