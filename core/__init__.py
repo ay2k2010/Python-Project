ad2fd0fc2a8d5f265ad3128576c4d981e98c4bdf
Philip Stachura <philip.stachura@unoworks.ca>
2017-05-11 12:02:12 -0700

    minor text fixes

--- core/__init__.py
+++ core/__init__.py
@@ -13,9 +13,9 @@ from shapes.test_shapes import *
 
 def test_shapes():
     """adds test shapes to window"""
-    add_object(ColourSquare(Point(20, 20), 10), True, True, True)
-    add_object(Spaceship(Point(50, 50), 10, 0.1, 0.4, 8, 0.3), True, False, True)
-    add_object(Face(Point(150, 150), 25))
+    # add_object(ColourSquare(Point(20, 20), 10), True, True, True)
+    # add_object(Spaceship(Point(50, 50), 10, 0.1, 0.4, 8, 0.3), True, False, True)
+    # add_object(Face(Point(150, 150), 25))
     add_object(Translator(Point(250, 250), 10), using_key_event=True)
 
 # call test function
