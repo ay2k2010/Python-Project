e773405c2056b9317348d2856d35eb14694afb10
Ethan Elliot Rajkumar <erdabravest@gmail.com>
2017-05-13 19:22:20 -0700

    added new class

--- core/__init__.py
+++ core/__init__.py
@@ -16,7 +16,8 @@ def test_shapes():
     # add_object(ColourSquare(Point(20, 20), 10), True, True, True)
     # add_object(Spaceship(Point(50, 50), 10, 0.1, 0.4, 8, 0.3), True, False, True)
     # add_object(Face(Point(150, 150), 25))
-    add_object(Translator(Point(250, 250), 10), using_key_event=True)
+    add_object(Spaceship(Point(250, 250), 10), using_key_event=True)
+
 
 # call test function
 test_shapes()
--- shapes/input_shapes.py
+++ shapes/input_shapes.py
@@ -1,6 +1,13 @@
 #test
 from core.advanced_shapes import *
-# from libs.graphics import *
+from libs.graphics import *
 
 class InputTextField(AdvancedShape):
     pass
+class Square(AdvancedShape):
+    def __init__(self,position,size):
+         self.square = Rectangle(Point(position.getX() - size, position.getY() - size),
+                                Point(position.getX() + size, position.getY() + size))
+    def display(self, window):
+        self.square.draw(window)
+
